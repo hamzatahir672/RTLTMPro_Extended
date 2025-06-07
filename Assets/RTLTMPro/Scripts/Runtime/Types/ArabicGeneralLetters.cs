6521850650e0710c914ac81911c237184859232a
hamzatahir672 <58887886+hamzatahir672@users.noreply.github.com>
2025-06-07 22:46:34 +0500

    Urdu Added

---
+++ Assets/RTLTMPro/Scripts/Runtime/Types/ArabicGeneralLetters.cs
@@ -0,0 +1,66 @@
+namespace RTLTMPro
+{
+    /// <summary>
+    /// Letters contextual forms - Isolated
+    /// </summary>
+    public enum ArabicGeneralLetters
+    {
+        Hamza = 0x0621, // ?
+        AlefMaddaAbove = 0x0622, // ?
+        AlefHamzaAbove = 0x0623, // ?
+        WawHamzaAbove = 0x0624,  // ?
+        AlefHamzaBelow = 0x0625, // ?
+        YehHamzaAbove = 0x0626,
+        Alef = 0x0627, // ?
+        Beh = 0x0628, // ?
+        TehMarbuta = 0x0629, // ?
+        Teh = 0x062A, // ?
+        Theh = 0x062B, // ?
+        Jeem = 0x062C, // ?
+        Hah = 0x062D, // ?
+        Khah = 0x062E, // ?
+        Dal = 0x062F, // ?
+        Thal = 0x0630, // ?
+        Reh = 0x0631, // ?
+        Zain = 0x0632, // ?
+        Seen = 0x0633, // ?
+        Sheen = 0x0634, // ?
+        Sad = 0x0635, // ?
+        Dad = 0x0636, // ?
+        Tah = 0x0637, // ?
+        Zah = 0x0638, // ?
+        Ain = 0x0639, // ?
+        Ghain = 0x063A, // ?
+        Feh = 0x0641, // ?
+        Qaf = 0x0642, // ?
+        Kaf = 0x0643, // ?
+        Lam = 0x0644, // ?
+        Meem = 0x0645, // ?
+        Noon = 0x0646, // ?
+        Heh = 0x0647, // ?
+        Waw = 0x0648, // ?
+        AlefMaksura = 0x649, // ?
+        Yeh = 0x064A, // ?
+
+        FarsiYeh = 0x6CC,
+        Peh = 0x067E, // persian ?
+        TCheh = 0x0686, // persian ?
+        Jeh = 0x0698, // persian ?
+        Keheh = 0x06A9, // persian ?
+        Gaf = 0x06AF, // persian ?
+
+        Tteh = 0x0679,
+        Ddal = 0x0688,
+        Rreh = 0x0691,
+        Bariyey = 0x06D2,
+        Hehdochasmi = 0x06BE, // urdu
+        Noongunna = 0x06BA, // urdu
+        UrduHeh = 0x06C1, // urdu
+
+        Tatweel = 0x640, // ?
+    }
+}
+
+
+//060C,061B,061F,0621,0622,0623,0624,0625,0626,0627,0628,0629,062A,062B,062C,062D,062E,062F,0630,0631,0632,0633,0634,0635,0636,0637,0638,0639,063A,0640,0641,0642,0643
+//,0644,0645,0646,0647,0648,0649,064A,067E,0686,0698,06A9,06AF,06CC,0679,0688,0691,06D2,06BE,06BA,06C1,FB56,FB57,FB58,FB59,FB7A,FB7B,FB7C,FB7D,FB8A,FB8B,FB8C,FB8D,FB92,FB93,FB94,FB95,FB8E,FB8F,FB90,FB91,FBFC - FBFF,FE70 - FEFF,064b - 065f,0670,FC5E - FC63,0660 - 0669,06F0 - 06F9
---
+++ Assets/RTLTMPro/Scripts/Runtime/Types/ArabicIsolatedLetters.cs
@@ -0,0 +1,65 @@
+namespace RTLTMPro
+{
+    /// <summary>
+    /// Arabic Contextual forms General - Unicode
+    /// </summary>
+    internal enum ArabicIsolatedLetters
+    {
+        Hamza = 0xFE80,
+        AlefMaddaAbove = 0xFE81,
+        AlefHamzaAbove = 0xFE83,
+        WawHamzaAbove = 0xFE85,
+        AlefHamzaBelow = 0xFE87,
+        YehHamzaAbove = 0xFE89,
+        Alef = 0xFE8D,
+        Beh = 0xFE8F,
+        TehMarbuta = 0xFE93,
+        Teh = 0xFE95,
+        Theh = 0xFE99,
+        Jeem = 0xFE9D,
+        Hah = 0xFEA1,
+        Khah = 0xFEA5,
+        Dal = 0xFEA9,
+        Thal = 0xFEAB,
+        Reh = 0xFEAD,
+        Zain = 0xFEAF,
+        Seen = 0xFEB1,
+        Sheen = 0xFEB5,
+        Sad = 0xFEB9,
+        Dad = 0xFEBD,
+        Tah = 0xFEC1,
+        Zah = 0xFEC5,
+        Ain = 0xFEC9,
+        Ghain = 0xFECD,
+        Feh = 0xFED1,
+        Qaf = 0xFED5,
+        Kaf = 0xFED9,
+        Lam = 0xFEDD,
+        Meem = 0xFEE1,
+        Noon = 0xFEE5,
+        Heh = 0xFEE9,
+        Waw = 0xFEED,
+        AlefMaksura = 0xFEEF,
+        Yeh = 0xFEF1,
+        
+        FarsiYeh = 0xFBFC,
+        Peh = 0xFB56,
+        TCheh = 0xFB7A,
+        Jeh = 0xFB8A,
+        Keheh = 0xFB8E, // persian kaf
+        Gaf = 0xFB92,
+
+        Tteh = 0xFB66,
+        Ddal = 0xFB88,
+        Rreh = 0xFB8C,
+        Bariyey = 0xFBAE,
+        Hehdochasmi= 0xFBAA, // urdu
+        Noongunna = 0xFB9E, // urdu
+        UrduHeh = 0xFBA6, // urdu
+
+    }
+}
+
+//060C,061B,061F,0621,0622,0623,0624,0625,0626,0627,0628,0629,062A,062B,062C,062D,062E,062F,0630,0631,0632,0633,0634,0635,0636,0637,0638,0639,063A,0640,0641,0642,0643
+//,0644,0645,0646,0647,0648,0649,064A,067E,0686,0698,06A9,06AF,06CC,0679,0688,0691,06D2,06BE,06BA,06C1,
+//FB56,FB57,FB58,FB59,FB7A,FB7B,FB7C,FB7D,FB8A,FB8B,FB8C,FB8D,FB92,FB93,FB94,FB95,FB8E,FB8F,FB90,FB91,FB66,FB88,FBAE,FBAA,FB9E,FBA6,FBFC - FBFF,FE70 - FEFF,064b - 065f,0670,FC5E - FC63,0660 - 0669,06F0 - 06F9
